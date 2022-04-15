b8040df5a00c74eb700223648f61a105f5b6ff5e
pkellner <peter@peterkellner.net>
2022-04-15 14:58:27 -0700

    format

--- src/cityStuff/CityDetail.js
+++ src/cityStuff/CityDetail.js
@@ -4,45 +4,60 @@ import { CityDetailStoreContext } from "../CityDetailStoreContext";
 function CityInfo() {
   const { resourceCityDetail } = useContext(CityDetailStoreContext);
   const city = resourceCityDetail.cityInfo.read();
-  return <div>{JSON.stringify(city)}</div>;
+  return (
+    <div className="border border-primary" >
+      name: {city[0].name}<br/>
+      state: {city[0].state}
+    </div>
+  )
+  //return <div>{JSON.stringify(city)}</div>;
 }
 
 function CityStats() {
   const { resourceCityDetail } = useContext(CityDetailStoreContext);
   const city = resourceCityDetail.cityStats.read();
-  return <div>{JSON.stringify(city)}</div>;
+  return (
+    <div className="border border-primary" >
+      City: {city[0].population}
+      <br />
+      Growth Since 2001: {city[0].growth}
+    </div>
+  );
 }
 
 function CityLocation() {
   const { resourceCityDetail } = useContext(CityDetailStoreContext);
   const city = resourceCityDetail.cityLocation.read();
-  return <div>{JSON.stringify(city)}</div>;
+  return (
+    <div className="border border-primary" >
+      latitude: {city[0].latitude}<br/>
+      longitude: {city[0].longitude}
+    </div>
+  )
+  //return <div>{JSON.stringify(city)}</div>;
 }
 
 export default function CityDetail() {
   return (
-    
-      <div className="grid">
-        <div className="row">
-          <Suspense fallback={<div>Loading CityInfo...</div>}>
-            <CityInfo />
-          </Suspense>
-        </div>
-        <div className="row">
-          <Suspense fallback={<div>Loading CityStats...</div>}>
-            <CityStats />
-          </Suspense>
-        </div>
-        <div className="row">
-          <Suspense fallback={<div>Loading CityLocation...</div>}>
-            <CityLocation />
-          </Suspense>
-        </div>
+    <div className="grid">
+      <div className="row">
+        <Suspense fallback={<div>Loading CityInfo...</div>}>
+          <CityInfo />
+        </Suspense>
       </div>
-   
+      <div className="row">
+        <Suspense fallback={<div>Loading CityStats...</div>}>
+          <CityStats />
+        </Suspense>
+      </div>
+      <div className="row">
+        <Suspense fallback={<div>Loading CityLocation...</div>}>
+          <CityLocation />
+        </Suspense>
+      </div>
+    </div>
   );
 }
-
 
 /*
 export default function CityDetail() {
